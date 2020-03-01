83a8f26d6bce07cd6c0c2a4d779e0b951181091e
Thng Yao Jie <yaoz1337@gmail.com>
2020-03-01 17:28:16 +0800

    Fixed minor build bugs in FXML files

--- src/test/java/seedu/jelphabot/testutil/TaskBuilder.java
+++ src/test/java/seedu/jelphabot/testutil/TaskBuilder.java
@@ -15,6 +15,7 @@ import seedu.jelphabot.model.util.SampleDataUtil;
 /**
  * A utility class to help with building Task objects.
  */
+// TODO rewrite classes referencing TaskBuilder
 public class TaskBuilder {
 
     public static final String DEFAULT_DESCRIPTION = "Individual Assignment 1";
