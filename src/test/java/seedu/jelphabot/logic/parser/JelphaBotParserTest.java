b1283a9bae7e5ad796c63f73c4c852713f9d825b
Thng Yao Jie <yaoz1337@gmail.com>
2020-03-01 16:19:22 +0800

    Fixed checkstyle violations

--- src/test/java/seedu/jelphabot/logic/parser/JelphaBotParserTest.java
+++ src/test/java/seedu/jelphabot/logic/parser/JelphaBotParserTest.java
@@ -57,7 +57,8 @@ public class JelphaBotParserTest {
         Task task = new TaskBuilder().build();
         EditPersonDescriptor descriptor = new EditPersonDescriptorBuilder(task).build();
         EditCommand command = (EditCommand) parser.parseCommand(EditCommand.COMMAND_WORD + " "
-                + INDEX_FIRST_PERSON.getOneBased() + " " + TaskUtil.getEditTaskDescriptorDetails(descriptor));
+                                                                + INDEX_FIRST_PERSON.getOneBased() + " " + TaskUtil
+                                                                        .getEditTaskDescriptorDetails(descriptor));
         assertEquals(new EditCommand(INDEX_FIRST_PERSON, descriptor), command);
     }
 
@@ -91,12 +92,14 @@ public class JelphaBotParserTest {
     @Test
     public void parseCommand_unrecognisedInput_throwsParseException() {
         assertThrows(ParseException.class, String.format(MESSAGE_INVALID_COMMAND_FORMAT, HelpCommand.MESSAGE_USAGE),
-                () -> parser.parseCommand(""));
+                     () -> parser.parseCommand("")
+        );
     }
 
     @Test
     public void parseCommand_unknownCommand_throwsParseException() {
         assertThrows(ParseException.class, MESSAGE_UNKNOWN_COMMAND,
-                () -> parser.parseCommand("unknownCommand"));
+                     () -> parser.parseCommand("unknownCommand")
+        );
     }
 }
