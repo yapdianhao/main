dd0da5839427060279fdda6598b8487b62ab8ef1
eedenong <e0325639@u.nus.edu>
2020-03-02 13:31:09 +0800

    Changed DateTime to reflect to user that only one date time format is supported, but kept support for other date time formats

--- src/main/java/seedu/jelphabot/model/task/DateTime.java
+++ src/main/java/seedu/jelphabot/model/task/DateTime.java
@@ -16,9 +16,8 @@ import java.util.List;
  */
 public class DateTime {
 
-    public static final String MESSAGE_CONSTRAINTS = "DateTime should be one of the formats:\n "
-                                                + "MMM-d-yyyy, MMM/d/yyyy HH mm, d/M/y HH mm, dd MM yyyy HH mm"
-                                                + "Time should be in the 24 hour format HH mm.";
+    public static final String MESSAGE_CONSTRAINTS = "Date should be of the format MMM-d-yyyy. "
+                                                         + "Time should be in the 24 hour format HH mm.";
     public final String value;
     private final DateFormat format;
 
@@ -41,10 +40,7 @@ public class DateTime {
      * @param test The date to be checked.
      * @return The boolean representing whether the date provided is valid.
      */
-    // TODO: Write test cases for different valid and invalid date time formats.
     public static boolean isValidDateTime(String test) {
-        //SimpleDateFormat sdf = new SimpleDateFormat("MMM-d-yyyy HH mm");
-        // SimpleDateFormat sdf2 = new SimpleDateFormat("dd MM yyyy HH mm");
         boolean correctFormat = false;
         for (String formatString : dateFormatStrings) {
             if (correctFormat) {
