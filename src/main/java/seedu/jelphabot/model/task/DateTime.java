ceb6787831081e5b5ccd61f3c98f4415afd66b40
eedenong <e0325639@u.nus.edu>
2020-03-02 09:59:43 +0800

    Added different supported date time formats, namely: MMM/d/yyyy HH mm, d/M/y HH mm, dd MM yyyy HH mm

--- src/main/java/seedu/jelphabot/model/task/DateTime.java
+++ src/main/java/seedu/jelphabot/model/task/DateTime.java
@@ -5,6 +5,8 @@ import static seedu.jelphabot.commons.util.AppUtil.checkArgument;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Class representing the Date and Time of the modelled task.
@@ -14,6 +16,9 @@ public class DateTime {
     public static final String MESSAGE_CONSTRAINTS = "DateTime should be of the format MMM-d-yyyy."
             +  "Time should be in the 24 hour format HH mm.";
     public final String value;
+
+    // TODO: Make UI display uniform date formatting, even with multiple date formats
+    private static final List<String> dateFormatStrings = Arrays.asList("MMM-d-yyyy HH mm", "MMM/d/yyyy HH mm", "d/M/y HH mm", "dd MM yyyy HH mm");
 
     /**
      * Constructs an {@code DateTime}.
@@ -27,21 +32,31 @@ public class DateTime {
     }
 
     /**
-     * Returns if the given string is a valid datetime.
+     * Returns if the given string is a valid datetime format, specified in the List dateFormatStrings.
      * @param test The date to be checked.
      * @return The boolean representing whether the date provided is valid.
      */
-    // TODO: check multiple date formats
+    // TODO: Write test cases for different valid and invalid date time formats.
     public static boolean isValidDateTime(String test) {
-        SimpleDateFormat sdf = new SimpleDateFormat("MMM-d-yyyy HH mm");
+        //SimpleDateFormat sdf = new SimpleDateFormat("MMM-d-yyyy HH mm");
         // SimpleDateFormat sdf2 = new SimpleDateFormat("dd MM yyyy HH mm");
-        sdf.setLenient(false);
-        try {
-            sdf.parse(test);
-        } catch (ParseException e) {
-            return false;
+        boolean correctFormat = false;
+        for (String formatString : dateFormatStrings) {
+            try {
+                new SimpleDateFormat(formatString).parse(test);
+                correctFormat = true;
+            } catch (ParseException e) {
+                continue;
+            }
         }
-        return true;
+//        sdf.setLenient(false);
+//        try {
+//            sdf.parse(test);
+//        } catch (ParseException e) {
+//            return false;
+//        }
+        //return true;
+        return correctFormat;
     }
 
     @Override
