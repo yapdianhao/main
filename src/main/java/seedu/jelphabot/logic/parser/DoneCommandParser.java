96acdab61509d861f7b0a7c8b8b985941eb59ab1
eedenong <e0325639@u.nus.edu>
2020-03-10 17:49:52 +0800

    Added relevant javadoc for DoneCommandParser

--- src/main/java/seedu/jelphabot/logic/parser/DoneCommandParser.java
+++ src/main/java/seedu/jelphabot/logic/parser/DoneCommandParser.java
@@ -6,9 +6,18 @@ import seedu.jelphabot.commons.core.index.Index;
 import seedu.jelphabot.logic.commands.DoneCommand;
 import seedu.jelphabot.logic.parser.exceptions.ParseException;
 
+/**
+ * Parses input arguments and creates a new DoneCommand object
+ */
 public class DoneCommandParser implements Parser<DoneCommand> {
 
-    @Override
+    /**
+     * Parses the given {@code String} of arguments in the context of DoneCommand
+     * and returns a DoneCommand object for execution.
+     * @param args user arguments.
+     * @return a DoneCommand object to be used for execution.
+     * @throws ParseException if the user input does not conform to the expected format
+     */
     public DoneCommand parse(String args) throws ParseException {
         try {
             Index index = ParserUtil.parseIndex(args);
