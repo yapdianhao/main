c69bf5267ca5fb5a1f3df676826fab83d4291edd
eedenong <e0325639@u.nus.edu>
2020-03-17 01:37:29 +0800

    Completed TaskCompletePredicateTest and TaskIncompletePredicateTest classes

--- src/test/java/seedu/jelphabot/model/task/TaskCompletedPredicateTest.java
+++ src/test/java/seedu/jelphabot/model/task/TaskCompletedPredicateTest.java
@@ -1,4 +1,34 @@
 package seedu.jelphabot.model.task;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.jelphabot.testutil.TaskBuilder;
+
 public class TaskCompletedPredicateTest {
+
+    @Test
+    public void equals() {
+        TaskCompletedPredicate predicate = new TaskCompletedPredicate();
+
+        // same object returns true
+        assertEquals(predicate, predicate);
+
+        // different types returns false
+        assertNotEquals(1, predicate);
+
+        // null returns false
+        assertNotEquals(null, predicate);
+    }
+
+    @Test
+    public void test_taskStatusEqual_returnsTrue() {
+        // true if the status of the Task is set to COMPLETED
+        TaskCompletedPredicate predicate = new TaskCompletedPredicate();
+        Task completedTask = new TaskBuilder().withStatus("COMPLETE").build();
+        assertTrue(predicate.test(completedTask));
+    }
 }
--- src/test/java/seedu/jelphabot/model/task/TaskIncompletePredicateTest.java
+++ src/test/java/seedu/jelphabot/model/task/TaskIncompletePredicateTest.java
@@ -1,4 +1,34 @@
 package seedu.jelphabot.model.task;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.jelphabot.testutil.TaskBuilder;
+
 public class TaskIncompletePredicateTest {
+
+    @Test
+    public void equals() {
+        TaskIncompletePredicate predicate = new TaskIncompletePredicate();
+
+        // same object returns true
+        assertEquals(predicate, predicate);
+
+        // different types returns false
+        assertNotEquals(1, predicate);
+
+        // null returns false
+        assertNotEquals(null, predicate);
+    }
+
+    @Test
+    public void test_taskStatusEqual_returnsTrue() {
+        // true if the status of the Task is set to INCOMPLETE
+        TaskIncompletePredicate predicate = new TaskIncompletePredicate();
+        Task incompleteTask = new TaskBuilder().withStatus("INCOMPLETE").build();
+        assertTrue(predicate.test(incompleteTask));
+    }
 }
