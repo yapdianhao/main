083c7867b1bedd680c5f8d4133f0ca46d3e33e5d
yapdianhao1011 <yapdhao@gmail.com>
2020-03-26 17:41:40 +0800

    added typicalreminders, reminderbuilder, remindertest

--- src/test/java/seedu/jelphabot/model/reminder/ReminderTest.java
+++ src/test/java/seedu/jelphabot/model/reminder/ReminderTest.java
@@ -1,1 +1,11 @@
 package seedu.jelphabot.model.reminder;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.jelphabot.testutil.ReminderBuilder;
+
+import static seedu.jelphabot.testutil.Assert.assertThrows;
+
+public class ReminderTest {
+
+}
