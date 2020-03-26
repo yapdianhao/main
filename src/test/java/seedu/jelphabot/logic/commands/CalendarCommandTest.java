2ddde4c84c0a136377791bf6757bb5ed491eb334
alam8064 <59945273+alam8064@users.noreply.github.com>
2020-03-26 23:02:18 +0800

    travis

--- src/test/java/seedu/jelphabot/logic/commands/CalendarCommandTest.java
+++ src/test/java/seedu/jelphabot/logic/commands/CalendarCommandTest.java
@@ -5,6 +5,11 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.jelphabot.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.jelphabot.testutil.TypicalTasks.getTypicalJelphaBot;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.junit.jupiter.api.Test;
 
 import seedu.jelphabot.model.Model;
@@ -12,19 +17,18 @@ import seedu.jelphabot.model.ModelManager;
 import seedu.jelphabot.model.UserPrefs;
 import seedu.jelphabot.model.task.predicates.TaskDueWithinDayPredicate;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
+/**
+ * Contains integration tests (interaction with the Model) for
+ * {@code CalendarCommand}.
+ */
 public class CalendarCommandTest {
     private Model model = new ModelManager(getTypicalJelphaBot(), new UserPrefs());
     private Model expectedModel = new ModelManager(getTypicalJelphaBot(), new UserPrefs());
 
     @Test
     public void execute_calendar_success() {
-        CommandResult expectedCommandResult = new CommandResult(CalendarCommand.MESSAGE_SWITCH_PANEL_ACKNOWLEDGEMENT, false,
-            false, false, true);
+        CommandResult expectedCommandResult = new CommandResult(CalendarCommand.MESSAGE_SWITCH_PANEL_ACKNOWLEDGEMENT,
+            false, false, false, true);
         assertCommandSuccess(new CalendarCommand(), model, expectedCommandResult, expectedModel);
     }
 
