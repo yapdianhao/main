074b644f7311fa47c3f0d5ecfccaedaa2e975195
Jel <42993921+Clouddoggo@users.noreply.github.com>
2020-03-27 22:15:56 +0800

    modify UI and abstract time spent to new class

--- src/main/java/seedu/jelphabot/model/task/Task.java
+++ src/main/java/seedu/jelphabot/model/task/Task.java
@@ -29,23 +29,23 @@ public class Task {
 
     private LocalDateTime startTime;
     private LocalDateTime endTime;
-    private Duration duration;
-    private boolean timerIsRunning;
+    private TimeSpent timeSpent;
+    private boolean isTiming;
 
     /**
      * Every field must be present and not null.
      */
     public Task(Description description, Status status, DateTime dateTime, ModuleCode moduleCode, Priority priority,
-                Set<Tag> tags, Duration duration) {
-        requireAllNonNull(description, status, dateTime, moduleCode, tags, duration);
+                Set<Tag> tags, TimeSpent timeSpent) {
+        requireAllNonNull(description, status, dateTime, moduleCode, tags, timeSpent);
         this.description = description;
         this.status = status;
         this.dateTime = dateTime;
         this.moduleCode = moduleCode;
         this.priority = priority;
         this.tags.addAll(tags);
-        this.duration = duration;
-        this.timerIsRunning = false;
+        this.timeSpent = timeSpent;
+        this.isTiming = false;
     }
 
     public Description getDescription() {
@@ -81,7 +81,7 @@ public class Task {
      */
     public void startTimer() {
         this.startTime = LocalDateTime.now();
-        this.timerIsRunning = true;
+        this.isTiming = true;
     }
 
     /**
@@ -89,24 +89,16 @@ public class Task {
      */
     public void stopTimer() {
         this.endTime = LocalDateTime.now();
-        this.setDuration();
-        this.timerIsRunning = false;
+        this.isTiming = false;
+        this.timeSpent.addTime(new TimeSpent(Duration.between(this.startTime, this.endTime)));
     }
 
-    private void setDuration() {
-        this.duration = this.duration.plus(Duration.between(this.startTime, this.endTime));
-    }
-
-    public Duration getDuration() {
-        return this.duration;
+    public TimeSpent getTimeSpent() {
+        return this.timeSpent;
     }
 
     public LocalDateTime getStartTime() {
         return this.startTime;
-    }
-
-    public LocalDateTime getEndTime() {
-        return this.endTime;
     }
 
     /**
@@ -130,7 +122,7 @@ public class Task {
      * Returns true if the timer is running for this task.
      */
     public boolean isBeingTimed() {
-        return this.timerIsRunning;
+        return this.isTiming;
     }
 
     /**
