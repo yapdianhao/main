93f99ed44edab693c3cf53bb1938c9d637ab959a
eedenong <e0325639@u.nus.edu>
2020-03-30 23:02:22 +0800

    Modified MainWindow to show Summary tab upon startup, with custom message.

--- src/main/java/seedu/jelphabot/ui/MainWindow.java
+++ src/main/java/seedu/jelphabot/ui/MainWindow.java
@@ -34,6 +34,10 @@ public class MainWindow extends UiPart<Stage> {
 
     private final Logger logger = LogsCenter.getLogger(getClass());
 
+    private static boolean firstStart = true;
+    private static final String WELCOME_STRING = "Welcome to JelphaBot! Here are the tasks that you have due today!\n"
+         + "To go back to the list of your tasks, type list!";
+
     private Stage primaryStage;
     private Logic logic;
 
@@ -239,9 +243,14 @@ public class MainWindow extends UiPart<Stage> {
      * Switches view to summary panel.
      */
     @FXML
-    private void handleSummary() {
+    public void handleSummary() {
         if (!summaryPanel.isShowing()) {
             summaryPanel.show();
+        }
+
+        if (firstStart) {
+            resultDisplay.setFeedbackToUser(WELCOME_STRING);
+            firstStart = false;
         }
     }
 
