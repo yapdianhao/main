527e72aa2dbf3992e4dd2b969c6b277598ae3c61
yapdianhao1011 <yapdhao@gmail.com>
2020-04-01 02:05:09 +0800

    write reminder test

--- src/test/java/seedu/jelphabot/model/reminder/ReminderTest.java
+++ src/test/java/seedu/jelphabot/model/reminder/ReminderTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.jelphabot.logic.commands.CommandTestUtil;
 import static seedu.jelphabot.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
