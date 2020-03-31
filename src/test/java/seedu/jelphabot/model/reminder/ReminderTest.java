f75f5ed4ed2d7349930ec3b9920170dc76ed5161
yapdianhao1011 <yapdhao@gmail.com>
2020-03-31 13:47:27 +0800

    started to write some tests

--- src/test/java/seedu/jelphabot/model/reminder/ReminderTest.java
+++ src/test/java/seedu/jelphabot/model/reminder/ReminderTest.java
@@ -1,1 +1,21 @@
 package seedu.jelphabot.model.reminder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.jelphabot.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.jelphabot.model.task.Task;
+import seedu.jelphabot.testutil.ReminderBuilder;
+import seedu.jelphabot.testutil.TaskBuilder;
+
+public class ReminderTest {
+
+    @Test
+    public void isSameReminder() {
+
+    }
+}
