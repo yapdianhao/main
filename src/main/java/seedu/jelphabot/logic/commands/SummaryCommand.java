1fcec8cc75989d7f40082336d1b485891828133c
Jel <42993921+Clouddoggo@users.noreply.github.com>
2020-04-01 18:07:10 +0800

    added shortcut commands

--- src/main/java/seedu/jelphabot/logic/commands/SummaryCommand.java
+++ src/main/java/seedu/jelphabot/logic/commands/SummaryCommand.java
@@ -6,10 +6,10 @@ import seedu.jelphabot.model.Model;
  * Switches to the summary panel.
  */
 public class SummaryCommand extends Command {
+
     public static final String COMMAND_WORD = "summary";
-
+    public static final String COMMAND_SHORTCUT = ":s";
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows summary panel.\n" + "Example: " + COMMAND_WORD;
-
     public static final String MESSAGE_SWITCH_PANEL_ACKNOWLEDGEMENT = "Switched to summary panel";
 
     @Override
