60e5db6d8e545677256bc8419ab67169d07337ce
Jel <42993921+Clouddoggo@users.noreply.github.com>
2020-04-01 14:29:50 +0800

    Saving files before refreshing line endings

--- src/main/java/seedu/jelphabot/ui/ProductivityCard.java
+++ src/main/java/seedu/jelphabot/ui/ProductivityCard.java
@@ -3,8 +3,6 @@ package seedu.jelphabot.ui;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 
