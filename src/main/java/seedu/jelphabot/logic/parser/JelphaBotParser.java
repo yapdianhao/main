7c55bf9eae440cc3800c3cdc2c79b612ec8eb94e
Jel <42993921+Clouddoggo@users.noreply.github.com>
2020-04-06 16:39:56 +0800

    remove extra import

--- src/main/java/seedu/jelphabot/logic/parser/JelphaBotParser.java
+++ src/main/java/seedu/jelphabot/logic/parser/JelphaBotParser.java
@@ -1,7 +1,6 @@
 package seedu.jelphabot.logic.parser;
 
 import static seedu.jelphabot.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.jelphabot.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.jelphabot.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 
 import java.util.regex.Matcher;
