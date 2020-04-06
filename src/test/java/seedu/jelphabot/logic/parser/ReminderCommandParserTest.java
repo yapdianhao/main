390fb09a58e725544b238424f970b36bc4bd4910
yapdianhao1011 <yapdhao@gmail.com>
2020-04-06 11:48:34 +0800

    done remindercommandparsertest

--- src/test/java/seedu/jelphabot/logic/parser/ReminderCommandParserTest.java
+++ src/test/java/seedu/jelphabot/logic/parser/ReminderCommandParserTest.java
@@ -2,6 +2,9 @@ package seedu.jelphabot.logic.parser;
 
 import static seedu.jelphabot.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.jelphabot.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.jelphabot.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.jelphabot.testutil.TypicalIndexes.INDEX_FIRST_TASK;
+import static seedu.jelphabot.testutil.TypicalReminders.ASSESSMENT_REMINDER;
 
 import org.junit.jupiter.api.Test;
 
@@ -18,5 +21,11 @@ public class ReminderCommandParserTest {
             String.format(MESSAGE_INVALID_COMMAND_FORMAT, ReminderCommand.MESSAGE_USAGE));
     }
 
+    @Test
+    public void parse_validArg_throwsParseException() {
+        ReminderCommand reminderCommand = new ReminderCommand(INDEX_FIRST_TASK, ASSESSMENT_REMINDER);
+        assertParseSuccess(parser, "1 days/1 hours/1", reminderCommand);
+        assertParseSuccess(parser, "   1  days/ 1 \n hours/  1 \t", reminderCommand);
+    }
 
 }
