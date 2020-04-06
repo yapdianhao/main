149215d23515a787458ccb2d6786d3c632a680bb
yapdianhao1011 <yapdhao@gmail.com>
2020-04-06 11:33:08 +0800

    removed controlsfx

--- src/test/java/seedu/jelphabot/logic/parser/ReminderCommandParserTest.java
+++ src/test/java/seedu/jelphabot/logic/parser/ReminderCommandParserTest.java
@@ -1,5 +1,22 @@
 package seedu.jelphabot.logic.parser;
 
+import static seedu.jelphabot.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.jelphabot.logic.parser.CommandParserTestUtil.assertParseFailure;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.jelphabot.logic.commands.ReminderCommand;
+
 public class ReminderCommandParserTest {
+
+    private ReminderCommandParser parser = new ReminderCommandParser();
+
+    @Test
+    public void parse_emptyArg_throwsParseException() {
+        assertParseFailure(
+            parser, " ",
+            String.format(MESSAGE_INVALID_COMMAND_FORMAT, ReminderCommand.MESSAGE_USAGE));
+    }
+
 
 }
